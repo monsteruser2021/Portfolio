44d22c67cf328a9fc7a229c9f854cbd0f69accd3
monsteruser <monsteruser2021@gmail.com>
2025-07-30 17:36:54 -0400

    minor fix

--- pages/components/TextShiny.tsx
+++ pages/components/TextShiny.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/pages/utils/cn";
+import { cn } from "@/utils/cn";
 import type { ComponentProps, ReactNode } from "react";
 // import "./shiny-text.css";
 const ShinyText = ({
